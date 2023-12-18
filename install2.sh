948a8fb60da6b13515c4351e1301458b99b8d48f
maximry2019 <52679336+maximry2019@users.noreply.github.com>
2023-12-18 05:07:03 +0200

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
+wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+mkdir ~/ccminer
+cd ~/ccminer
+GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/Android-Mining/releases?per_page=1" | jq -c '[.[] | del (.body)]')
+GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .browser_download_url")
+GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets | .[] | .name")
+
+echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
+
+wget ${GITHUB_DOWNLOAD_URL} -O ~/ccminer/ccminer
+wget https://raw.githubusercontent.com/maximry2019/VerusCliMining/main/config.json -O ~/ccminer/config.json
+wget https://raw.githubusercontent.com/maximry2019/VerusCliMining/main/config2.json -O ~/ccminer/config2.json
+chmod +x ~/ccminer/ccminer
+
+cd ..
+mkdir ~/xmrig
+cd ~/xmrig
+
+wget https://raw.githubusercontent.com/maximry2019/VerusCliMining/main/xmrig -O ~/xmrig/xmrig
+wget https://raw.githubusercontent.com/maximry2019/VerusCliMining/main/config-xmr.json -O ~/xmrig/config.json
+
+cat << EOF > ~/xmrig/start.sh
+#!/bin/sh
+~/xmrig/xmrig -o de.zephyr.herominers.com:1123 -u ZEPHYR2KyQca6cZXXqKMFHiVxUNSPPnAnWYsmtwTm2mT61VTkWRs8BcTZdWKQRcE5pRDZbLhhV2XCVrzoHUL6H5jaKsfoERLpQR5N -p S8-1 -a rx/0 -k -t 8
+EOF
+chmod +x start.sh
+
+cat << EOF > ~/ccminer/start.sh
+#!/bin/sh
+~/ccminer/ccminer -c ~/ccminer/config.json
+EOF
+chmod +x start.sh
+
+cat << EOF > ~/ccminer/start2.sh
+#!/bin/sh
+~/ccminer/ccminer -c ~/ccminer/config2.json
+EOF
+chmod +x start2.sh
+
+echo "setup nearly complete."
+echo "Edit the config with \"nano ~/ccminer/config.json\""
+echo "Or edit the config2 with \"nano ~/ccminer/config2.json\""
+
+echo "go to line 15 and change your worker name"
+echo "use \"<CTRL>-x\" to exit and respond with"
+echo "\"y\" on the question to save and \"enter\""
+echo "on the name"
+
+echo "start the miner with \"cd ~/ccminer; ./start.sh\"."
+echo "or start the miner with \"cd ~/ccminer; ./start2.sh\"."
