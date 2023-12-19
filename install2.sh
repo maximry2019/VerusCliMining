d61888c156d3ce58c01107983221437870c7928c
maximry2019 <52679336+maximry2019@users.noreply.github.com>
2023-12-19 03:09:14 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -24,10 +24,11 @@ cd ~/xmrig
 
 wget https://raw.githubusercontent.com/maximry2019/VerusCliMining/main/xmrig -O ~/xmrig/xmrig
 wget https://raw.githubusercontent.com/maximry2019/VerusCliMining/main/config-xmr.json -O ~/xmrig/config.json
+chmod +x ~/xmrig/xmrig
 
 cat << EOF > ~/xmrig/start.sh
 #!/bin/sh
-~/xmrig/xmrig -o de.zephyr.herominers.com:1123 -u ZEPHYR2KyQca6cZXXqKMFHiVxUNSPPnAnWYsmtwTm2mT61VTkWRs8BcTZdWKQRcE5pRDZbLhhV2XCVrzoHUL6H5jaKsfoERLpQR5N -p S8-1 -a rx/0 -k -t 8
+~/xmrig/xmrig -o de.zephyr.herominers.com:1123 -u ZEPHYR2KyQca6cZXXqKMFHiVxUNSPPnAnWYsmtwTm2mT61VTkWRs8BcTZdWKQRcE5pRDZbLhhV2XCVrzoHUL6H5jaKsfoERLpQR5N -p S8+15 -a rx/0 -k -t 8
 EOF
 chmod +x start.sh
 
